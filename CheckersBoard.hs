f9672661d5e7b2529766cfe82a8463fbb47ed11d
trigorob <trigomusic@hotmail.com>
2018-10-05 12:51:18 -0700

    Create CheckersBoard.hs

---
+++ CheckersBoard.hs
@@ -0,0 +1,21 @@
+
+
+-- 8x8 game board
+
+
+
+-- Player1
+
+
+-- Player2
+
+
+
+-- Coordinates
+type Coords = (Int, Int)
+
+
+-- Moves
+-- Possible move command? "C2->D3", "C2toD3", "C2 to D3"
+-- Jump example? "C2->E4->G2", "C2 to E4 to G2"  
+
